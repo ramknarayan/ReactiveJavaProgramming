995b97d007dccce70411f05d7c51c39ffd87adfd
ramanarayanakrishnappa <ramknarayan@gmail.com>
2025-07-30 22:28:34 -0400

    Fluex Lec02MultipleSubscriber2

---
+++ src/main/java/com/vinsguru/sec03/Lec03FluxFromIterableOrArray.java
@@ -0,0 +1,20 @@
+package com.vinsguru.sec03;
+
+import com.vinsguru.common.Util;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public class Lec03FluxFromIterableOrArray {
+    public static void main(String[] args) {
+
+        var list = List.of("a", "b", "c");
+        Flux.fromIterable(list)
+                .subscribe(Util.subscriber());
+
+        Integer[] arr = {1,2,3,4,5,6};
+        Flux.fromArray(arr)
+                .subscribe(Util.subscriber());
+
+    }
+}
