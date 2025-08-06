fd765dbf37dc523062747ffa6c997c35923d0d80
ramanarayanakrishnappa <ramknarayan@gmail.com>
2025-08-05 23:05:46 -0400

    Fluex Lec02MultipleSubscriber2

--- src/main/java/com/vinsguru/sec03/client/ExternalServiceClient.java
+++ src/main/java/com/vinsguru/sec03/client/ExternalServiceClient.java
@@ -1,16 +1,16 @@
 package com.vinsguru.sec03.client;
 
 import com.vinsguru.common.AbstractHttPClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class ExternalServiceClient extends AbstractHttPClient {
 
-    public Mono<String> getProductName(int productId){
+    public Flux<String> getNames(){
         return this.httpClient.get()
-                .uri("/demo01/product/"+ productId)
+                .uri("/demo02/name/stream")
                 .responseContent()
-                .asString()
-                .next();
+                .asString();
 
     }
 
