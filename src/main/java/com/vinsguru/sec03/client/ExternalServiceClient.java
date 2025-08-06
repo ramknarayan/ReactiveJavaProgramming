41662a814cda73340a6d9f0fe40f506b7e91d30d
Kaliraj Arumugam <kaliraj@gmail.com>
2025-08-06 08:10:22 +0530

    New files are committed

---
+++ src/main/java/com/vinsguru/sec03/client/ExternalServiceClient.java
@@ -0,0 +1,17 @@
+package com.vinsguru.sec03.client;
+
+import com.vinsguru.common.AbstractHttPClient;
+import reactor.core.publisher.Mono;
+
+public class ExternalServiceClient extends AbstractHttPClient {
+
+    public Mono<String> getProductName(int productId){
+        return this.httpClient.get()
+                .uri("/demo01/product/"+ productId)
+                .responseContent()
+                .asString()
+                .next();
+
+    }
+
+}
